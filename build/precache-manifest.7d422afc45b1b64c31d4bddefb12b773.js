57de55556e326143ad9795c43800ec9a990e86f3
Dhanesh <dhaneshraj2311@gmail.com>
2022-02-27 15:17:34 +0530

    Added public and gh-pages

---
+++ build/precache-manifest.7d422afc45b1b64c31d4bddefb12b773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5216186c845814d28ac",
+    "url": "/dhaneshrajpm/E-comApplication/static/css/main.e89fcf1d.chunk.css"
+  },
+  {
+    "revision": "c5216186c845814d28ac",
+    "url": "/dhaneshrajpm/E-comApplication/static/js/main.de8409f5.chunk.js"
+  },
+  {
+    "revision": "c4b8e4384ce8f07c6c69",
+    "url": "/dhaneshrajpm/E-comApplication/static/js/runtime~main.db0228c9.js"
+  },
+  {
+    "revision": "4cbbdf06ab68679095cb",
+    "url": "/dhaneshrajpm/E-comApplication/static/js/2.6d94f2b2.chunk.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/dhaneshrajpm/E-comApplication/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/dhaneshrajpm/E-comApplication/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "5ee9466155b0314bb0abfa174797530b",
+    "url": "/dhaneshrajpm/E-comApplication/index.html"
+  }
+];
