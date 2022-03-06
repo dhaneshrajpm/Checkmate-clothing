ebd8c734d1e986425a3a40e02b3856a32196b448
Dhanesh <dhaneshraj2311@gmail.com>
2022-03-06 21:36:12 +0530

    Updates

---
+++ precache-manifest.49835728cb5fbe1ae073379a43eff6a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "207ee3251584bd4e9bfe",
+    "url": "/Checkmate-clothing/static/css/main.1c974947.chunk.css"
+  },
+  {
+    "revision": "207ee3251584bd4e9bfe",
+    "url": "/Checkmate-clothing/static/js/main.c9b77f73.chunk.js"
+  },
+  {
+    "revision": "0ef8167252fa693afa76",
+    "url": "/Checkmate-clothing/static/js/runtime~main.4a6a3627.js"
+  },
+  {
+    "revision": "504b5a6a43578c72bc85",
+    "url": "/Checkmate-clothing/static/js/2.d9e4649b.chunk.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Checkmate-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Checkmate-clothing/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "671660c7ded454bf054988ef97197add",
+    "url": "/Checkmate-clothing/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/E-comApplication/precache-manifest.647ed95ab4250b40b73f6347d3349f22.js"
+  "/Checkmate-clothing/precache-manifest.49835728cb5fbe1ae073379a43eff6a3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/E-comApplication/index.html", {
+workbox.routing.registerNavigationRoute("/Checkmate-clothing/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
